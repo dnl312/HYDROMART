1874c3a68a98d3fb9f0c644372d179a00d1f18e5
dnl312 <danielpj1234@gmail.com>
2025-01-17 22:22:25 +0700

    nyicil repo

--- auth/go.mod
+++ auth/go.mod
@@ -1,3 +1,31 @@
 module auth
 
 go 1.23.2
+
+require (
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/google/uuid v1.6.0
+	golang.org/x/crypto v0.17.0
+	google.golang.org/grpc v1.69.4
+	gorm.io/driver/postgres v1.5.11
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+)
---
+++ auth/repo/auth.go
@@ -0,0 +1,99 @@
+package repo
+
+import (
+	"auth/config"
+	"auth/model"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-playground/validator"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	Db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return UserRepository{
+		Db: db,
+	}
+}
+
+func (u *UserRepository) LoginUser(user model.User) (string, error) {
+	tokenString := ""
+
+	var userGet model.User
+	if err := config.DB.Table("users_hydromart").Where("username = ?", user.Username).First(&userGet).Error; err != nil {
+		if err == gorm.ErrRecordNotFound{
+			return tokenString, status.Errorf(codes.NotFound, "user %s not found %s", user.Username, user.Password)
+		}
+		return tokenString, status.Error(codes.Internal, err.Error())
+	}
+
+	errString := fmt.Sprintf("invalid username or password for user %s password %s", user.Username, user.Password)
+	if err := bcrypt.CompareHashAndPassword( []byte(userGet.Password), []byte(user.Password)); err != nil {
+		return errString, status.Errorf(codes.Unauthenticated, err.Error())
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userid": userGet.UserID,
+		"username": userGet.Username,
+		"role": userGet.Role,
+		"exp" : jwt.TimeFunc().Add(time.Hour * 72).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return tokenString, status.Error(codes.Internal, err.Error())
+	}
+
+	return tokenString, nil
+}
+
+func validateRegisterUser(user model.RegisterUser) error {
+	log.Printf("Validating user: %+v", user)
+    validate := validator.New()
+    err := validate.Struct(user)
+    if err != nil {
+        for _, e := range err.(validator.ValidationErrors) {
+            switch e.Field() {
+            default:
+                return fmt.Errorf("%s is %s", e.Field(), e.Tag())
+            }
+        }
+    }
+    return nil
+}
+
+func (u UserRepository) RegisterUser(user model.RegisterUser) error {
+
+	err := validateRegisterUser(user)
+	if err != nil {
+		return status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return status.Error(codes.Internal, err.Error())
+	}	
+
+	newUser := model.User{
+		UserID:       uuid.New().String(),
+		Username: user.Username,
+		Password: string(hashedPassword),
+	}
+
+	if err := config.DB.Table("users_hydromart").Create(&newUser).Error; err != nil {
+		return status.Error(codes.Internal, err.Error())
+	}
+
+	return nil
+}
