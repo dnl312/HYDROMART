996778811bfbb03b0d8bfdab297388b35a5c43ed
Ohkaaa <mhmmdlintangwp28@gmail.com>
2025-01-20 11:02:03 +0700

    on progress

--- user/main.go
+++ user/main.go
@@ -1,1 +1,31 @@
 package user
+
+import (
+	"log"
+	"user/config"
+	"user/controller"
+	"user/repo"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	db, err := config.InitDB()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	config.ClearPreparedStatements()
+	log.Printf("Connected to database")
+	defer config.CloseDB()
+
+	userRepository := repo.NewUserRepository(db)
+	userController := controller.NewMerchantController(&userRepository)
+
+	config.ListenAndServeGrpc(&userController)
+}
