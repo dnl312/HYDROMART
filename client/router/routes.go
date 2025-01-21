2e3ff99d69aaec5171d6998feb29228bd16bfc22
Ohkaaa <mhmmdlintangwp28@gmail.com>
2025-01-21 14:02:26 +0700

    in progress users service

--- client/router/routes.go
+++ client/router/routes.go
@@ -6,11 +6,18 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func Echo(e *echo.Echo, uc controller.AuthController) {
+func Echo(e *echo.Echo, uc controller.AuthController, oc controller.OrderController) {
 	users := e.Group("/users")
-	{		
+	{
 		users.POST("/login", uc.LoginUser)
 		users.POST("/register", uc.RegisterUser)
+	}
+
+	orders := e.Group("/orders")
+	{
+		orders.POST("/create", oc.CreateOrder)
+		orders.GET("/", oc.GetAllOrders)
+		orders.DELETE("/", oc.DeleteOrder)
 	}
 
 }
